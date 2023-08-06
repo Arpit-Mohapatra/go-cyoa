ade93d152aa0490190032577b28e03e5c796faa8
Arpit Mohapatra <mohapatra.arpit2003@cet.edu.in>
2023-08-06 20:04:35 +0530

    intro html template rendering

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/Arpit-Mohapatra/cyoa"
@@ -10,6 +12,7 @@ import (
 
 func main() {
 	filename := flag.String("file", "../../gopher.json", "JSON file with the CYOA story")
+	port := flag.Int("port", 3000, "port to start CYOA application on")
 	flag.Parse()
 	fmt.Printf("Using the story in %s\n", *filename)
 
@@ -22,6 +25,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	
-	fmt.Printf("%+v\n", story)
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting serer at port:%d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- story.go
+++ story.go
@@ -2,9 +2,48 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
 
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTmpl = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choose Your Own Adventure</title>
+</head>
+<body>
+   <h1>{{.Title}}</h1>
+    {{range .Paragraph}}
+        <p>{{.}}</p>
+    {{end}}
+    <ul>
+        {{range .Options}}
+            <li> <a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+    </ul>
+</body>
+</html>`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
 
 func JsonStory(r io.Reader) (Story, error) {
 	var story Story
@@ -26,4 +65,8 @@ type Chapter struct {
 type Option struct {
 	Text    string `json:"text"`
 	Chapter string `json:"arc"`
+}
+
+type handler struct {
+	s Story
 }
